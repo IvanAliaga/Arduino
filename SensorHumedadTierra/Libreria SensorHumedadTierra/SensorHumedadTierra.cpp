e36bdb95c5b60f73d54f6c4141f357821b227d16
Ivan Aliaga <ivanaliaga22@gmail.com>
2013-10-06 02:45:21 +0200

    Update SensorHumedadTierra.cpp

--- SensorHumedadTierra/Libreria SensorHumedadTierra/SensorHumedadTierra.cpp
+++ SensorHumedadTierra/Libreria SensorHumedadTierra/SensorHumedadTierra.cpp
@@ -1,5 +1,5 @@
 /*
-  SensorHumedadTierra.cpp -Descripción cpp
+  SensorHumedadTierra.cpp -DescripciÃ³n cpp
   Creada por Ivan Aliaga, el 6 de Octubre del 2013
   Lanzado bajo licencia Apache v2 License
 */
@@ -7,36 +7,40 @@
 #include "arduino.h"
 #include "SensorHumedadTierra.h"
 
+int valor = 0;
 SensorHumedadTierra::SensorHumedadTierra(int variableAnaloga, int delay)
 {
-  _variableAnaloga = analogRead(variableAnaloga);
+  _variableAnaloga = variableAnaloga;
   _delay = delay;
 }
 void SensorHumedadTierra::getValorAnalogo()
 {
-  Serial.println(_variableAnaloga);
+  valor = analogRead(_variableAnaloga);
+  Serial.println(valor);
 }
 void SensorHumedadTierra::getResultado()
-{
-  if (_variableAnaloga <= 373)
-        Serial.println(" Encharcado");
-      if ((_variableAnaloga > 373) && (_variableAnaloga <= 796))
-        Serial.println(" Humedo, no regar");
-      if (_variableAnaloga > 796)
-        Serial.println(" Seco, necesitas regar");
-      if(_variableAnaloga == 1023)
-	Serial.println(" El sensor esta fuera de la tierra");
+{ 
+  valor = analogRead(_variableAnaloga);
+  if (valor <= 373)
+        Serial.println("Encharcado");
+      if ((valor > 373) && (valor <= 796))
+        Serial.println("Humedo, no regar");
+      if (valor > 796)
+        Serial.println("Seco, necesitas regar");
+      if(valor == 1023)
+	Serial.println("El sensor esta fuera de la tierra");
       delay(_delay);
 }
 void SensorHumedadTierra::getVariableResultado()
-{
-  if (_variableAnaloga <= 373)
+{ 
+  valor = analogRead(_variableAnaloga);
+  if (valor <= 373)
         Serial.println("E");
-      if ((_variableAnaloga > 373) && (_variableAnaloga <= 796))
+      if ((valor > 373) && (valor <= 796))
         Serial.println("H");
-      if (_variableAnaloga > 796)
+      if (valor > 796)
         Serial.println("S");
-      if(_variableAnaloga == 1023)
+      if(valor == 1023)
 	Serial.println("O");
       delay(_delay);
 }
